36f38fb1d844bdbc5b901471eb0bfccd60bdd8fd
PoojaB26 <pbhaumik26@gmail.com>
2018-05-11 20:38:30 +0530

    Set up Firebase ML vision; working stage

---
+++ app/src/main/java/com/poojab26/textrecognizer/GraphicUtils/TextGraphic.java
@@ -0,0 +1,65 @@
+package com.poojab26.textrecognizer.GraphicUtils;
+
+/**
+ * Created by poojab26 on 11-May-18.
+ */
+
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.Log;
+import com.poojab26.textrecognizer.GraphicUtils.GraphicOverlay.Graphic;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+
+/**
+ * Graphic instance for rendering TextBlock position, size, and ID within an associated graphic
+ * overlay view.
+ */
+public class TextGraphic extends Graphic {
+
+    private static final String TAG = "TextGraphic";
+    private static final int TEXT_COLOR = Color.RED;
+    private static final float TEXT_SIZE = 54.0f;
+    private static final float STROKE_WIDTH = 4.0f;
+
+    private final Paint rectPaint;
+    private final Paint textPaint;
+    private final FirebaseVisionText.Element element;
+
+    public TextGraphic(GraphicOverlay overlay, FirebaseVisionText.Element element) {
+        super(overlay);
+
+        this.element = element;
+
+        rectPaint = new Paint();
+        rectPaint.setColor(TEXT_COLOR);
+        rectPaint.setStyle(Paint.Style.STROKE);
+        rectPaint.setStrokeWidth(STROKE_WIDTH);
+
+        textPaint = new Paint();
+        textPaint.setColor(TEXT_COLOR);
+        textPaint.setTextSize(TEXT_SIZE);
+        // Redraw the overlay, as this graphic has been added.
+        postInvalidate();
+    }
+
+    /**
+     * Draws the text block annotations for position, size, and raw value on the supplied canvas.
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        Log.d(TAG, "on draw text graphic");
+        if (element == null) {
+            throw new IllegalStateException("Attempting to draw a null text.");
+        }
+
+        // Draws the bounding box around the TextBlock.
+        RectF rect = new RectF(element.getBoundingBox());
+        canvas.drawRect(rect, rectPaint);
+
+        // Renders the text at the bottom of the box.
+        canvas.drawText(element.getText(), rect.left, rect.bottom, textPaint);
+    }
+}
--- app/src/main/java/com/poojab26/textrecognizer/MainActivity.java
+++ app/src/main/java/com/poojab26/textrecognizer/MainActivity.java
@@ -1,6 +1,7 @@
 package com.poojab26.textrecognizer;
 
 import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+import com.poojab26.textrecognizer.GraphicUtils.GraphicOverlay;
+import com.poojab26.textrecognizer.GraphicUtils.TextGraphic;
 import com.wonderkiln.camerakit.CameraKit;
 import com.wonderkiln.camerakit.CameraKitError;
 import com.wonderkiln.camerakit.CameraKitEvent;
@@ -15,6 +24,8 @@ import com.wonderkiln.camerakit.CameraKitEventListener;
 import com.wonderkiln.camerakit.CameraKitImage;
 import com.wonderkiln.camerakit.CameraKitVideo;
 import com.wonderkiln.camerakit.CameraView;
+import com.poojab26.textrecognizer.GraphicUtils.GraphicOverlay.Graphic;
+
 
 import java.util.List;
 
@@ -22,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     CameraView cameraView;
     ImageView mImageCam;
     Button mBtnCamera;
+    GraphicOverlay mGraphicOverlay;
     private static final int INPUT_SIZE = 224;
 
     @Override
@@ -31,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         cameraView = findViewById(R.id.camView);
         mImageCam = findViewById(R.id.imgCamera);
         mBtnCamera = findViewById(R.id.cameraBtn);
+        mGraphicOverlay = findViewById(R.id.graphic_overlay);
 
         cameraView.addCameraKitListener(new CameraKitEventListener() {
             @Override
@@ -49,8 +62,9 @@ public class MainActivity extends AppCompatActivity {
                 Bitmap bitmap = cameraKitImage.getBitmap();
 
                 bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
+                mGraphicOverlay.clear();
                 mImageCam.setImageBitmap(bitmap);
-
+                runTextRecognition(bitmap);
 
 
             }
@@ -82,4 +96,50 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
     }
 
+
+    private void runTextRecognition(Bitmap bitmap) {
+        Log.d("TAG", "In function");
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionTextDetector detector = FirebaseVision.getInstance()
+                .getVisionTextDetector();
+        //mButton.setEnabled(false);
+        detector.detectInImage(image)
+                .addOnSuccessListener(
+                        new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText texts) {
+                               // mButton.setEnabled(true);
+                                processTextRecognitionResult(texts);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                //mButton.setEnabled(true);
+                                e.printStackTrace();
+                            }
+                        });
+    }
+
+    private void processTextRecognitionResult(FirebaseVisionText texts) {
+        List<FirebaseVisionText.Block> blocks = texts.getBlocks();
+        if (blocks.size() == 0) {
+            Log.d("TAG", "No text found");
+            return;
+        }
+        mGraphicOverlay.clear();
+        for (int i = 0; i < blocks.size(); i++) {
+            List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
+            for (int j = 0; j < lines.size(); j++) {
+                List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
+                for (int k = 0; k < elements.size(); k++) {
+                    Graphic textGraphic = new TextGraphic(mGraphicOverlay, elements.get(k));
+                    mGraphicOverlay.add(textGraphic);
+
+                }
+            }
+        }
+    }
 }
